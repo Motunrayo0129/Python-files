3e9c0f5acd27edb7de8167293a446d5ade36a3e8
Motunrayo0129 <onibudomotunrayo2901@gmail.com>
2025-05-13 16:28:54 +0100

    may13 chapter3 assignment

---
+++ Class_Work_Python - Copy/studentscore.py
@@ -0,0 +1,21 @@
+student_num = int(input('Enter the number of student: '))
+top_name = ""
+top_score = None
+second_top_name =""
+second_top_score = None
+for score in range(student_num):
+	student_name = input("Enter student name: ")
+	student_score = int(input("Enter student score: "))
+
+	if top_score is None or score > top_score:
+		second_top_score = top_score
+		second_top_name = top_name
+
+		top_score = student_score
+		top_name = student_name
+	elif second_top_score is None or score > second_top_score:
+		second_top_score = student_score
+		second_top_name = student_name
+
+print(f"{top_name} has the highest score of {top_score} and")
+print(f"{second_top_name} has the second highest score of {second_top_score}")
---
+++ missingcode.py
@@ -0,0 +1,5 @@
+for num in range(5):
+	for num2 in range(8):
+		print(' @', end = '')
+	print()
+	
